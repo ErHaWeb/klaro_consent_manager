7a9b66dc0f6abb618fa6e4a7c0a33370a037209d
Eric Harrer <info@eric-harrer.de>
2024-02-05 16:52:18 +0100

    [TASK] Additions to ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * This file is part of the "klaro_consent_manager" Extension for TYPO3 CMS.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Klaro! Consent Manager',
     'description' => 'Functionally complete, flexible TYPO3 integration of Klaro! Consent Management by KIProtect. Klaro! is a powerful tool that protects your visitors\' privacy and data and helps you run a GDPR compliant website.',
@@ -8,14 +21,13 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'info@eric-harrer.de',
     'author_company' => 'eric-harrer.de',
     'state' => 'stable',
+    'clearCacheOnLoad' => true,
     'version' => '1.7.1',
     'constraints' => [
         'depends' => [
             'typo3' => '10.4.0-12.4.99',
             'php' => '7.4.0-8.2.99',
         ],
-        'conflicts' => [],
-        'suggests' => [],
     ],
     'autoload' => [
         'psr-4' => [
