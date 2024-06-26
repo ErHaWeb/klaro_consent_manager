0eb8d731cbd34d00bb27789aba47ec072ae908db
Eric Harrer <info@eric-harrer.de>
2024-06-26 23:47:23 +0200

    [TASK] Update PHP Prologue

--- Classes/Utility/ExtensionConfigurationUtility.php
+++ Classes/Utility/ExtensionConfigurationUtility.php
@@ -1,7 +1,7 @@
 <?php declare(strict_types=1);
 
 /*
- * This file is part of the TYPO3 CMS project.
+ * This file is part of the "klaro_consent_manager" Extension for TYPO3 CMS.
  *
  * It is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License, either version 2
