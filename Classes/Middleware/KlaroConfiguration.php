f205ab730e203736fd57d4a661b6396c72d4b9fb
Eric Harrer <info@eric-harrer.de>
2024-06-27 11:15:16 +0200

    !!![TASK] Modernize retrieval of standalone klaro configuration
    `ErHaWeb\KlaroConsentManager\Middleware\KlaroConfiguration` now replaces the former TypoScript PAGE object with typeNum 1699541845 in order to be able to call up the klaro configuration directly. The general entry point for the call is now `/klaro-config.js`.

---
+++ Classes/Middleware/KlaroConfiguration.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the "klaro_consent_manager" Extension for TYPO3 CMS.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace ErHaWeb\KlaroConsentManager\Middleware;
+
+use ErHaWeb\KlaroConsentManager\Service\KlaroService;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use TYPO3\CMS\Core\Http\Response;
+use TYPO3\CMS\Core\Http\Stream;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class KlaroConfiguration implements MiddlewareInterface
+{
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        if ($request->getRequestTarget() === '/klaro-config.js') {
+            $klaroService = GeneralUtility::makeInstance(KlaroService::class, $request);
+            $configuration = $klaroService->getRawConfiguration();
+            if ($configuration) {
+                $body = new Stream('php://temp', 'wb+');
+                $body->write($klaroService->getConfigurationInlineJavaScript());
+
+                return new Response(
+                    $body,
+                    200,
+                    ['Content-Type' => 'application/javascript; charset=utf-8']
+                );
+            }
+        }
+        return $handler->handle($request);
+    }
+}
--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -1,9 +1,19 @@
 <?php declare(strict_types=1);
 
+use ErHaWeb\KlaroConsentManager\Middleware\KlaroConfiguration;
 use ErHaWeb\KlaroConsentManager\Middleware\ReplaceBeforeOutput;
 
 return [
     'frontend' => [
+        'erhaweb/klaro-consent-manager/klaro-configuration' => [
+            'target' => KlaroConfiguration::class,
+            'before' => [
+                'typo3/cms-frontend/eid',
+            ],
+            'after' => [
+                'typo3/cms-frontend/site'
+            ],
+        ],
         'erhaweb/klaro-consent-manager/replace-content' => [
             'target' => ReplaceBeforeOutput::class,
             'before' => [
