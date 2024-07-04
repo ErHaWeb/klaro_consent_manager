310e1efafad35445779e2285f0d4c5bddedfd0ef
Eric Harrer <info@eric-harrer.de>
2024-07-04 09:52:04 +0200

    [TASK] Move TypoScript files to Site Set directory

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,4 +1,6 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 /*
  * This file is part of the "klaro_consent_manager" Extension for TYPO3 CMS.
@@ -13,34 +15,20 @@
  * The TYPO3 project - inspiring people to share!
  */
 
+use TYPO3\CMS\Core\Information\Typo3Version;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 defined('TYPO3') || die();
 
 (static function () {
-    /**
-     * Extension key
-     */
-    $extKey = 'klaro_consent_manager';
-
-    /**
-     * TypoScript path
-     */
-    $path = 'Configuration/TypoScript';
-
-    /**
-     * Locallang file path
-     */
-    $locallangFilePath = 'LLL:EXT:' . $extKey . '/Resources/Private/Language/locallang_db.xlf';
-
-    /**
-     * Static TypoScript include title
-     */
-    $title = $locallangFilePath . ':sys_template.TypoScript.' . $extKey . '_title';
-
-    /**
-     * Add static TypoScript (constants and setup) directly through TCA instead of the API function to be able to translate the title
-     */
-    if (is_array($GLOBALS['TCA']['sys_template']['columns'])) {
-        $value = str_replace(',', '', 'EXT:' . $extKey . '/' . $path);
-        $GLOBALS['TCA']['sys_template']['columns']['include_static_file']['config']['items'][] = [$title, $value];
+    $extensionKey = 'klaro_consent_manager';
+    $versionInformation = GeneralUtility::makeInstance(Typo3Version::class);
+    if ($versionInformation->getMajorVersion() < 13) {
+        ExtensionManagementUtility::addStaticFile(
+            $extensionKey,
+            'Configuration/Sets/KlaroConsentManager',
+            'Klaro! Consent Manager',
+        );
     }
 })();
