2c3ad3fcb01767475a421841617c77f48749d6e1
Eric Harrer <info@eric-harrer.de>
2026-04-29 15:12:32 +0200

    [TASK] Add missing strict types declaration

--- Configuration/TCA/tx_klaroconsentmanager_cookie.php
+++ Configuration/TCA/tx_klaroconsentmanager_cookie.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     'ctrl' => [
         'title' => 'LLL:EXT:klaro_consent_manager/Resources/Private/Language/locallang_db.xlf:tx_klaroconsentmanager_cookie',
--- ext_emconf.php
+++ ext_emconf.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * This file is part of the "klaro_consent_manager" Extension for TYPO3 CMS.
  *
