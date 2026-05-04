3ed178b3d112157575195319c7bb312704bda39e
Eric Harrer <info@eric-harrer.de>
2026-05-04 23:33:23 +0200

    [BUGFIX] Keep ext_emconf.php clean

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,19 +1,5 @@
 <?php
-
-declare(strict_types=1);
-
-/**
- * This file is part of the "klaro_consent_manager" Extension for TYPO3 CMS.
- *
- * It is free software; you can redistribute it and/or modify it under
- * the terms of the GNU General Public License, either version 2
- * of the License, or any later version.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- * The TYPO3 project - inspiring people to share!
- */
+´
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Klaro! Consent Manager',
     'description' => 'Functionally complete, flexible TYPO3 integration of Klaro! Consent Management by KIProtect GmbH, a powerful tool that protects your visitors\' privacy and data.',
